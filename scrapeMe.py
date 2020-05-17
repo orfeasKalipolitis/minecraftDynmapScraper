f056bf11ad97f4d54d47ffa91b8ca1e5fa84c9db
Orfeas Kalipolitis <orfeaskalipolitis@gmail.com>
2020-05-17 16:29:10 +0200

    Write info to file using JSON

--- scrapeMe.py
+++ scrapeMe.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup as BS
 import time
+import json
 
 # fixing up the url
 baseURL = 'http://minecraft.spelenshus.se'
@@ -18,9 +19,27 @@ driver.get(url)
 
 # parse every hour
 while True:
+    # get that soup
     soup = BS(driver.page_source, 'lxml')
     ul = soup.find_all('ul')
     # ul[4] contains the players currently connected
+    #
+    # get those players
     players = ul[4].find_all('li')
-    print('There are: ' + str(len(players)) + ' players currently connected.')
+    #
+    # get all that pertinent info
+    entry = {}
+    entry['numOfPlayers'] = str(len(players))
+    entry['timestamp'] = time.strftime("%Y-%m-%d %H:%M:%S")
+    entry['playerNames'] = []
+    for player in players:
+        entry['playerNames'].append(player.find(title='Center on player').text)
+    #
+    # write all of that info down
+    with open('ServerStatistics.log', 'a') as outputFile:
+        json.dump(entry, outputFile, sort_keys=True, indent=4)
+        outputFile.write(',\n')
+    print(entry['timestamp'] + ': ' + str(len(players)) + ' players currently online on classic survival.')
+    #
+    # good job, go to sleep now
     time.sleep(interval)
